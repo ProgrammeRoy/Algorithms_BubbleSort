c482eb03e6189b8dcdafb792835b33b905307ea7
samu marz <samuel.martinez.ramirez@gmail.com>
2018-11-15 13:06:08 -0500

    Add Sort like App Algoritms

--- src/Main.kt
+++ src/Main.kt
@@ -1,21 +1,50 @@
 fun main(args: Array<String>){
     var listNumbers = arrayListOf(4,45,36,44,36,67,27,58,79,10)
-    var maxCantidad:Int = listNumbers.count()
-    var number:Int = 0
-    var rest: Int = 2
 
-    while (maxCantidad>1){
+    BubbleSort_FromApp(listNumbers)
+    BubbleSort_MyImplementation(listNumbers)
+}
 
-        for (i in 0..listNumbers.size-rest ){
-            if (listNumbers[i]<listNumbers[i+1]){
-                number = listNumbers[i+1]
-                listNumbers[i+1] = listNumbers[i]
-                listNumbers[i] = number
+fun BubbleSort_FromApp(list:ArrayList<Int>) {
+    var listNumberMI = list
+    var maxCantidadMI:Int = listNumberMI.count()
+    var numberMI = 0
+    var restMI = 2
+
+    while (maxCantidadMI>1){
+
+        for (i in (listNumberMI.size) downTo 2 ){
+            //If you change Asc to Desc change < to >
+            if (listNumberMI[i-1]<listNumberMI[i-2]){
+                numberMI = listNumberMI[i-2]
+                listNumberMI[i-2] = listNumberMI[i-1]
+                listNumberMI[i-1] = numberMI
             }
         }
-        rest++
-        maxCantidad--
+        restMI++
+        maxCantidadMI--
     }
+    println(listNumberMI)
+}
 
-    println(listNumbers)
+fun BubbleSort_MyImplementation(list:ArrayList<Int>) {
+    var listNumberMI = list
+    var maxCantidadMI:Int = listNumberMI.count()
+    var numberMI = 0
+    var restMI = 2
+
+    while (maxCantidadMI>1){
+
+        for (i in 0..listNumberMI.size-restMI ){
+            //If you change Asc to Desc change < to >
+            if (listNumberMI[i]>listNumberMI[i+1]){
+                numberMI = listNumberMI[i+1]
+                listNumberMI[i+1] = listNumberMI[i]
+                listNumberMI[i] = numberMI
+            }
+        }
+        restMI++
+        maxCantidadMI--
+    }
+    println(listNumberMI)
 }
