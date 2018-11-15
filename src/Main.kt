98b3f71cc83bf387f96cebab78921830126dfbc6
samu marz <samuel.martinez.ramirez@gmail.com>
2018-11-15 16:40:47 -0500

    Fix Sort App, go down necesary

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,20 @@
 fun main(args: Array<String>){
-    var listNumbers = arrayListOf(2,32,52,46,35,63,74,56,43,52,11,17)
+    val listNumbers = arrayListOf(2,32,52,46,35,63,74,56,43,52,11,17)
 
     BubbleSort_FromApp(listNumbers)
     BubbleSort_MyImplementation(listNumbers)
 }
 
 fun BubbleSort_FromApp(list:ArrayList<Int>) {
-    var listNumberMI = list
+    val listNumberMI = list
     var maxCantidadMI:Int = listNumberMI.count()
-    var numberMI = 0
+    var numberMI:Int
     var restMI = 0
 
     while (maxCantidadMI>1){
 
         for (i in (listNumberMI.size-1) downTo 1+restMI ){
-            //If you change Asc to Desc change < to >
+            //If you change Asc to Desc change < to > ..
             if (listNumberMI[i]>listNumberMI[i-1]){
                 numberMI = listNumberMI[i-1]
                 listNumberMI[i-1] = listNumberMI[i]
@@ -28,15 +28,15 @@ fun BubbleSort_FromApp(list:ArrayList<Int>) {
 }
 
 fun BubbleSort_MyImplementation(list:ArrayList<Int>) {
-    var listNumberMI = list
+    val listNumberMI = list
     var maxCantidadMI:Int = listNumberMI.count()
-    var numberMI = 0
+    var numberMI:Int
     var restMI = 2
 
     while (maxCantidadMI>1){
 
         for (i in 0..listNumberMI.size-restMI ){
-            //If you change Asc to Desc change < to >
+            //If you change Asc to Desc change < to > ..
             if (listNumberMI[i]>listNumberMI[i+1]){
                 numberMI = listNumberMI[i+1]
                 listNumberMI[i+1] = listNumberMI[i]
